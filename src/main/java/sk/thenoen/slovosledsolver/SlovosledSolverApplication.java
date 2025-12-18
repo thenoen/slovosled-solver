ea0f4c37dd17f8b43208b81240ea0a43fe92a4ae
marek <kordikmarek@gmail.com>
2025-12-18 10:27:26 +0100

    improved logging - do not log game even if the same max score is achieved, there are too many such games

--- src/main/java/sk/thenoen/slovosledsolver/SlovosledSolverApplication.java
+++ src/main/java/sk/thenoen/slovosledsolver/SlovosledSolverApplication.java
@@ -156,7 +156,7 @@ public class SlovosledSolverApplication implements CommandLineRunner {
 				final Game game = new Game(tiles, bonus, selectedWords, wordSelectionCombination);
 				final long score = game.play();
 
-				if (score >= bestScore) {
+				if (score > bestScore) {
 					bestScore = score;
 //					logger.info("Found best score: {} for word combination {}", bestScore, selectedWords);
 					logger.info("Found best score: {} for word combination:", bestScore);
